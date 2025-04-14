42947edf7ca11f3116c34358fd77ad5de431d1cb
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2025-04-14 13:23:31 +0530

    Create Find Pair with Target Sum - Hard version.py

---
+++ Day 22/Find Pair with Target Sum - Hard version.py
@@ -0,0 +1,18 @@
+https://my.newtonschool.co/playground/code/h0m8gsozm7lu
+
+
+def has_pair_with_sum(arr, target):
+    left = 0
+    right = len(arr) - 1
+    
+    while left < right:
+        current_sum = arr[left] + arr[right]
+        
+        if current_sum == target:
+            return True
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+    
+    return False
