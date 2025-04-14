ca5ed34ea493d3fb852a414a180d069eb69dac15
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2025-04-14 13:39:32 +0530

    Create 100 Days of Code Begins.py

---
+++ Day 1/100 Days of Code Begins.py
@@ -0,0 +1,5 @@
+https://my.newtonschool.co/playground/code/w0metkrdhn2z
+
+
+n = int(input())
+print("Congratulations! You've completed Day 1!") if n == 1 else print(f"You're on Day {n} of the challenge! Keep going!")
