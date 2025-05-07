7051afaddf0e6597f0d7b8e5a2191641abb3409a
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2025-05-07 11:43:01 +0530

    Update Average of Levels.py

--- Day 82/Average of Levels.py
+++ Day 82/Average of Levels.py
@@ -1,4 +1,26 @@
 https://my.newtonschool.co/playground/code/3vjucl4oma03
 
 
-Solution: On the way 😊
+def average_of_levels(root):
+    ans = []
+    if not root:
+        return ans
+    
+    q = deque([root])
+    
+    while q:
+        level_sum = 0
+        level_count = len(q)
+        
+        for _ in range(level_count):
+            node = q.popleft()
+            level_sum += node.val
+            
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        
+        ans.append(level_sum / level_count)
+    
+    return ans
