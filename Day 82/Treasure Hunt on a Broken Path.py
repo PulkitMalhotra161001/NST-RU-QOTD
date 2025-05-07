302407632a048a755e6a75c8ffda8f529fe822d6
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2025-05-07 11:42:41 +0530

    Update Treasure Hunt on a Broken Path.py

--- Day 82/Treasure Hunt on a Broken Path.py
+++ Day 82/Treasure Hunt on a Broken Path.py
@@ -1,3 +1,19 @@
 https://my.newtonschool.co/playground/code/8ww1b6hw77h8
 
-Solution: On the way 😊
+
+def max_treasure(caves, k):
+    n = len(caves)
+    dp = [0] * (n + 1)   
+    for i in range(1, n + 1):
+        skip = dp[i - 1]
+        take = caves[i - 1]
+        if i - k - 1 >= 0:
+            take += dp[i - k - 1]
+        dp[i] = max(skip, take)
+    
+    return dp[n]
+
+n, k = map(int, input().split())
+caves = list(map(int, input().split()))
+result = max_treasure(caves, k)
+print(result)
